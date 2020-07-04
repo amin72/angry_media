0cfaeecd7d628eb9c1f155bea05d9a0a8388c0e2
Amin <asaa47721@gmail.com>
2020-07-05 04:23:01 +0430

    change pagination class from PageNumberPagination to LimitOffsetPagination

--- backend/post/paginations.py
+++ backend/post/paginations.py
@@ -1,7 +1,6 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 
-class StandardPagination(PageNumberPagination):
-    page_size = 15
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+class StandardPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 10
